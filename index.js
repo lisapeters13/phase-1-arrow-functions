c519a3e442fb22bb4f52f303984333ddd63459ff
lisapeters13 <lisapetersirl@gmail.com>
2024-03-14 17:25:06 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let divide = function() {
+    return 2000 / 100;
+  };
+  
+  let result = divide();
+  console.log(result); // Output: 20
+
+  const square = (num) => num * num;
+
+  const add = (num1, num2) => num1 + num2;
